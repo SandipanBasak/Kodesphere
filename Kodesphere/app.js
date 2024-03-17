84b4ee2ae5ad0c4b85955f0eb4af175d97763e09
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-03-17 11:45:42 +0530

    connected mongoose

--- Kodesphere/app.js
+++ Kodesphere/app.js
@@ -1,1 +1,18 @@
-const app=require('express');
+const express=require('express');
+ const mongoose=require('mongoose');
+const app=express();
+const port=3000;
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://rssmp120:rohan3046@cluster0.nsofrmr.mongodb.net/Kodespehere');
+
+const devices=mongoose.model('devices',{
+    teamid:String,
+    device:String,
+    value:Number
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server is Running on port ${port}`);
+})
